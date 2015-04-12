4fa3f75dc9337bd7b88509d7ec4e88fb7125fc19
pars555 <pars5555@yahoo.com>
2015-04-12 19:10:29 +0400

    123

---
+++ classes/actions/main/GetImageAction.class.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once(CLASSES_PATH . "/actions/BaseAction.class.php");
+
+/**
+ * @author Vahagn Sookiasian
+ * @site http://naghashyan.com
+ * @mail vahagnsookaisyan@gmail.com
+ * @year 2010-2012
+ */
+class GetImageAction extends BaseAction {
+
+    public function service() {
+        $serialNumber = $this->args[0];
+        $this->showImage(IMG_ROOT_DIR . '/' . $serialNumber . '/image.jpg');
+    }
+
+    private function showImage($picture) {
+        if (file_exists($picture)) {
+            header('Content-type: image/jpg');
+            imagejpeg($picture);
+        }
+    }
+
+    public function getRequestGroup() {
+        return RequestGroups::$guestRequest;
+    }
+
+}
+
+?>
