0c2a682b26967dab0d24cdc96cc9d33a7b0402cc
pars555 <pars5555@yahoo.com>
2015-04-12 18:10:44 +0400

    123

--- classes/actions/device/ImageAction.class.php
+++ classes/actions/device/ImageAction.class.php
@@ -12,11 +12,30 @@ class ImageAction extends BaseAction {
             $this->error(array('message' => "No Serial Number!"));
         }
         $token = $this->secure($_REQUEST['token']);
+        $this->saveImage($token);
+
         $this->ok();
     }
 
     public function getRequestGroup() {
         return RequestGroups::$guestRequest;
+    }
+
+    public function saveImage($token) {
+        $originalPictureFullName = null;
+        $file_name = $_FILES[$fileName]['name'];
+        $tmp_name = $_FILES[$fileName]['tmp_name'];
+        $file_size = $_FILES[$fileName]['size'];
+
+        $dir = IMG_ROOT_DIR . "/devices/";
+        if (!is_dir($dir)) {
+            mkdir($dir, 0777);
+        }
+        $dir = IMG_ROOT_DIR . "/devices/" . $token;
+        if (!is_dir($dir)) {
+            mkdir($dir, 0777);
+        }
+        move_uploaded_file($tmp_name, $dir.'/image.jpg');
     }
 
 }
