0f36075d619538ee44b7950493e0e2bcf75c622e
pars555 <pars5555@yahoo.com>
2015-04-12 18:56:51 +0400

    3456

--- htdocs/js/ngs/loads/carwash/DevicesLoad.class.js
+++ htdocs/js/ngs/loads/carwash/DevicesLoad.class.js
@@ -34,7 +34,7 @@ ngs.DevicesLoad = Class.create(ngs.AbstractLoad, {
     },
     reloadImgs: function (token) {
         var path = jQuery('#device_' + token + "_img").attr('base_path');
-        jQuery('#device_' + token + "_img").attr('src', '');
+        //jQuery('#device_' + token + "_img").attr('src', '');
         jQuery('#device_' + token + "_img").attr('src', path + "?dummy=" + Date.now());
     },
     updatePage: function () {
